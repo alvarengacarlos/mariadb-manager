791196a0f1059c5adb6c8490efec61b9da3baf60
alvarengacarlos <carlosho200@hotmail.com>
2022-07-02 13:05:47 -0400

    feat: Create StatementOperation class

---
+++ src/core/statement/StatementOperation.js
@@ -0,0 +1,13 @@
+class StatementOperation {
+
+    executeStatement(statement){
+        try {
+            //execute statement
+        } catch(exception) {
+            console.error(exception);
+        }
+    }
+    
+}
+
+module.exports = StatementOperation;
