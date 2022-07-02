f5258ecdd6b7cd5baab8fdca03f7d9c4652f3fcf
alvarengacarlos <carlosho200@hotmail.com>
2022-07-02 11:58:00 -0400

    feat: create AccountManagement class

---
+++ src/core/statement/account/AccountManagement.js
@@ -0,0 +1,68 @@
+class AccountManagement {
+    
+    /**
+     * 
+     * @param {String} username 
+     * @param {String} password 
+     * @param {String} host
+     * @returns {String}
+     */
+    createUser(username, password, host) {
+        if (!username || !password || !host) {
+            throw new Error("username, password and host cannot be empty");
+        }
+
+        return `CREATE USER IF NOT EXISTS ${username}@${host} IDENTIFIED BY ${password};`;
+    }
+
+    /**
+     * 
+     * @param {String} username 
+     * @param {String} password 
+     * @param {String} host
+     * @returns {String}
+     */
+    alterUser(username, password, host) {
+        if (!username || !password || !host) {
+            throw new Error("username, password and host cannot be empty");
+        }
+
+        return `ALTER USER IF EXISTS ${username}@${host} IDENTIFIED BY ${password};`;
+    }
+
+    /**
+     * 
+     * @param {String} username 
+     * @returns {String}
+     */
+    dropUser(username) {
+        if (!username) {
+            throw new Error("username cannot be empty");
+        }
+
+        return `DROP USER IF EXISTS ${username};`;
+    }
+
+    /**
+     * 
+     * @param {String} oldUsername 
+     * @param {String} oldHost 
+     * @param {String} newUsername 
+     * @param {String} newHost 
+     * @returns {String}
+     */
+    renameUser(oldUsername, oldHost, newUsername, newHost) {
+        if (!oldUsername || !oldHost || !newUsername || !newHost) {
+            throw new Error("oldUsername, oldHost, newUsername and newHost cannot be empty");
+        }
+
+        return `RENAME USER ${oldUsername}@${oldHost} TO ${newUsername}@${newHost};`;
+    }
+
+    revoke() {}
+    createRole() {}
+    dropRole() {}
+    showGrants() {}
+}
+
+module.exports = AccountManagement;
---
+++ src/core/statement/account/AccountManagement.test.js
@@ -0,0 +1,61 @@
+const {it, describe, before, beforeEach} = require("mocha");
+const expect = require("chai").expect;
+
+const AccountManagement = require("./AccountManagement");
+
+describe("core/statement/account/AccountManagement.js", () => {
+
+    let username, password, host;
+    before(() => {
+        username = "myusername";
+        password = "mypassword";
+        host = "localhost";
+    });
+
+    let account;
+    beforeEach(() => {
+        account = new AccountManagement();
+    });
+    
+    it("given a username, a password and a host when executed the createUser method then it must create statement to create user", () => {
+        const statement = account.createUser(username, password, host);
+
+        expect(statement)
+            .to.eql(`CREATE USER IF NOT EXISTS ${username}@${host} IDENTIFIED BY ${password};`);
+        
+        expect(statement).to.be.string
+    });
+
+    it("given a username and password when executed the alterUser method then it must create statement to alter user", () => {
+        const statement = account.alterUser(username, password, host);
+
+        expect(statement)
+            .to.eql(`ALTER USER IF EXISTS ${username}@${host} IDENTIFIED BY ${password};`);
+
+        expect(statement).to.be.string
+    });
+
+    it("given a username when executed the dropUser method then it must create statement to drop a user", () => {
+        const statement = account.dropUser(username);
+
+        expect(statement)
+            .to.eql(`DROP USER IF EXISTS ${username};`);
+        
+        expect(statement).to.be.string;
+    });
+
+    it("given a old username, old password, new username and new password when executed the renameUser method then it must create statement to rename the account", () => {
+        const oldUsername = "pascal";
+        const oldPassword = "pascalpw";
+        const newUsername = "java";
+        const newPassword = "javapw";
+
+        const statement = account.renameUser(oldUsername, oldPassword, newUsername, newPassword);
+
+        expect(statement)
+            .to.eql(`RENAME USER ${oldUsername}@${oldPassword} TO ${newUsername}@${newPassword};`);
+        
+        expect(statement).to.be.string;
+    });
+
+});
